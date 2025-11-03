98d344c0d260225f253e3537bdcae2aea788966d
zkinhang <zkinhang714@gmail.com>
2025-11-03 21:23:13 +0800

    Remove k3s installation script

--- k3s_installation_server.sh
+++
@@ -1,2 +0,0 @@
-curl -sfL https://get.k3s.io | K3S_URL=https://192.168.10.130:6443 K3S_TOKEN=K10598227a7e8c784daa2e77eefc6b06d8e95f17a2a33dfd1bc08e2585264916b09::server:7a732348b37953ed53249bda560c97eb sh -
-
