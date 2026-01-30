fbe35c887f922494fe7854fd8818731fd7c0cfd2
zkinhang <zkinhang714@gmail.com>
2026-01-30 15:39:43 +0800

    refactor: add archived script for micro-ROS image build and update build_and_copy_to_local_registry.sh

--- build_and_copy_to_local_registry.sh
+++ build_and_copy_to_local_registry.sh
@@ -1,3 +1,5 @@
+# Build the main image and push to remote registry (dockerhub)
 docker buildx build --platform linux/amd64,linux/arm64 -f docker/Dockerfile -t zkinhang/manta-ray-ros:latest --push .
 
+# Copy the image from dockerhub to local registry
 skopeo copy --all docker://docker.io/zkinhang/manta-ray-ros:latest docker://mantaray.local:5000/manta-ray-ros:latest --dest-tls-verify=false
