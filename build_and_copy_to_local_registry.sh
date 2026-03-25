4a4c773b24ad615d7d9476b67671e42041bac37d
zkinhang <zkinhang714@gmail.com>
2026-03-26 01:46:00 +0800

    Add Dockerfile for web UI with multi-stage build for Nginx deployment
    - Create a Dockerfile that builds the web UI using Node.js and serves it with Nginx.
    - Use a multi-stage build to optimize the final image size.
    - Install dependencies and build the application in the first stage.
    - Copy the built assets to the Nginx image for serving.
    - Expose port 80 for web traffic.

--- build_and_copy_to_local_registry.sh
+++ build_and_copy_to_local_registry.sh
@@ -1,5 +1,11 @@
 # Build the main image and push to remote registry (dockerhub)
-docker buildx build --platform linux/amd64,linux/arm64 -f docker/Dockerfile -t zkinhang/manta-ray-ros:latest --push .
+docker buildx build --platform linux/amd64,linux/arm64 -f docker/common/Dockerfile -t zkinhang/manta-ray-ros:latest --push .
 
-# Copy the image from dockerhub to local registry
+# Build the microRos image and push to remote registry (dockerhub)
+docker buildx build --platform linux/amd64,linux/arm64 -f docker/microros/Dockerfile -t zkinhang/microros-with-esptool:latest --push .
+
+# Copy the main image from dockerhub to local registry
 skopeo copy --all docker://docker.io/zkinhang/manta-ray-ros:latest docker://mantaray.local:5000/manta-ray-ros:latest --dest-tls-verify=false
+
+# Copy the microRos image from dockerhub to local registry
+skopeo copy --all docker://docker.io/zkinhang/microros-with-esptool:latest docker://mantaray.local:5000/microros-with-esptool:latest --dest-tls-verify=false
