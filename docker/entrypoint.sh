7f1d0ff70dfd3cdcc9182e49796931796dfb8f86
zkinhang <zkinhang714@gmail.com>
2026-03-27 18:18:54 +0800

    Refactor code structure for improved readability and maintainability

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -5,5 +5,7 @@ set -e
 source /opt/ros/jazzy/setup.bash
 source /ros2_ws/install/setup.bash
 
+export PYTHONPATH="/ros2_ws/.venv/lib/python3.12/site-packages:$PYTHONPATH"
+
 # Execute the command passed into the container
 exec "$@"
